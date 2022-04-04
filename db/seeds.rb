8018d8ff9b68d898e19a20c4a656c6c5ce2eea83
BK <benkoshy@hotmail.com>
2022-04-04 13:08:04 +1000

    add: post scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+1.upto(500).each do
+  Post.create
+end
